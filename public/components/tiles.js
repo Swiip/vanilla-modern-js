4dac92d0f8da3ad437110129693d504c13d4907e
Swiip <matthieu.lux@gmail.com>
2018-06-05 22:00:15 +0200

    create a framework api

--- public/components/tiles.js
+++ public/components/tiles.js
@@ -1,169 +1,55 @@
-import { range } from "/utils/utils.js";
+import {
+  component,
+  withProp,
+  withMarkup,
+  withHandler,
+  withConnected,
+  withStore
+} from "/framework/component.js";
 
-import { render } from "/framework/render.js";
+import "/components/tile.js";
 
-import { store } from "/logic/connector.js";
-
-// im port { size } from "/game/conf.js";
-const size = 4;
-
-const template = `
-  <root></root>
-
-  <style>
-  .tile {
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    border-radius: 3px;
-    z-index: 10;
-    font-weight: bold;
-    font-size: 55px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    transition: all .3s ease;
-    animation: .3s appear;
-  }
-
-  .merged {
-    z-index: 9;
-  }
-
-  .tile2 {
-    background-color: #eee4da;
-  }
-
-  .tile4 {
-    background-color: #ede0c8;
-  }
-
-  .tile8 {
-    color: #f9f6f2;
-    background: #f2b179;
-  }
-
-  .tile16 {
-    color: #f9f6f2;
-    background: #f59563;
-  }
-
-  .tile32 {
-    color: #f9f6f2;
-    background: #f67c5f;
-  }
-
-  .tile64 {
-    color: #f9f6f2;
-    background: #f65e3b;
-  }
-
-  .tile128 {
-    color: #f9f6f2;
-    background: #edcf72;
-    font-size: 45px;
-  }
-
-  .tile256 {
-    color: #f9f6f2;
-    background: #edcc61;
-    font-size: 45px;
-  }
-
-  .tile512 {
-    color: #f9f6f2;
-    background: #edc850;
-    font-size: 45px;
-  }
-
-  .tile1024 {
-    color: #f9f6f2;
-    background: #edc53f;
-    font-size: 35px;
-  }
-
-  .tile2048 {
-    color: #f9f6f2;
-    background: #edc22e;
-    font-size: 35px;
-  }
-
-   @keyframes appear {
-     from {
-       height: 0;
-       width: 0;
-       opacity: 0;
-       margin-top: 50px;
-       margin-left: 50px;
-     }
-     to {
-       height: 100px;
-       width: 100px;
-       opacity: 1;
-       margin-top: 0;
-       margin-left: 0;
-     }
-   }
-  </style>
-`;
-
-customElements.define(
+component(
   "swiip-tiles",
-  class Tiles extends HTMLElement {
-    constructor() {
-      super();
+  withStore(({ board }) => {
+    const tiles = [];
 
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = template;
-
-      this.root = this.shadowRoot.querySelector("root");
+    if (!board) {
+      return tiles;
     }
 
-    connectedCallback() {
-      // console.log("tiles", worker);
-      store.subscribe(() => this.updateTiles());
-      this.updateTiles();
-    }
-
-    updateTiles() {
-      const { board } = store.getState();
-
-      if (!board) {
-        return;
-      }
-
-      const tiles = [];
-
-      board.forEach(rows => {
-        rows.forEach(cell => {
-          if (cell.value > 0) {
-            tiles.push(cell);
-          }
-          if (Array.isArray(cell.mergedTiles)) {
-            tiles.push(...cell.mergedTiles);
-          }
-        });
+    board.forEach(rows => {
+      rows.forEach(cell => {
+        if (cell.value > 0) {
+          tiles.push(cell);
+        }
+        if (Array.isArray(cell.mergedTiles)) {
+          tiles.push(...cell.mergedTiles);
+        }
       });
+    });
 
-      const newHtml = `
-        <div class="tile-container">
-          ${tiles
-            .map(
-              tile =>
-                `<div
-                  key="${tile.id}"
-                  class="tile tile${tile.value} ${tile.merged ? "merged" : ""}"
-                  style="top: ${20 + 120 * tile.row}px; left: ${20 +
-                  120 * tile.column}px;">
-                  ${tile.value}
-                </div>`
-            )
-            .join("")}
-        </div>
-      `;
-
-      render(this.root, newHtml);
-    }
-  }
+    return { tiles };
+  }),
+  withMarkup(
+    ({ tiles = [] }) => `
+      <div>
+        ${tiles
+          .map(
+            tile => `
+              <swiip-tile
+                key="${tile.id}"
+                row="${tile.row}"
+                col="${tile.column}"
+                merged="${tile.merged}"
+                value="${tile.value}"
+              >
+                ${tile.value}
+              </swiip-tile>
+            `
+          )
+          .join("")}
+      </div>
+    `
+  )
 );
--- public/framework/component.js
+++ public/framework/component.js
@@ -1,38 +1,75 @@
 import { compose } from "/utils/utils.js";
 import { render } from "/framework/render.js";
+import { store } from "/logic/connector.js";
 
 export const component = (name, ...enhancers) => {
-  const customElement = class extends HTMLElement {};
+  const customElement = class extends HTMLElement {
+    connectedCallback() {}
+    attributeChangedCallback() {}
+    update() {}
+  };
   customElements.define(name, compose(...enhancers)(customElement));
 };
+
+export const withProp = name => Base =>
+  class extends Base {
+    static get observedAttributes() {
+      return [...(super.observedAttributes || []), name];
+    }
+    attributeChangedCallback(attributeName, oldValue, newValue) {
+      super.attributeChangedCallback(attributeName, oldValue, newValue);
+      if (attributeName === name) {
+        this[name] = newValue;
+        this.update();
+      }
+    }
+  };
 
 export const withMarkup = handler => Base =>
   class extends Base {
     connectedCallback() {
-      if (super.connectedCallback) {
-        super.connectedCallback();
-      }
+      super.connectedCallback();
       this.attachShadow({ mode: "open" });
+      render(this.shadowRoot, handler(this));
+    }
+    update() {
+      super.update();
       render(this.shadowRoot, handler(this));
     }
   };
 
-export const withStyle = (...args) => Base => {
-  const tag = args.length === 1 ? "div" : args[0];
-  const handler = args.length === 1 ? args[0] : args[1];
+export const withStyle = handler => Base => {
+  let styleNode;
+
+  const createStyle = component => {
+    const node = document.createElement("style");
+    node.appendChild(
+      document.createTextNode(`
+        :host { display: block }
+        ${handler(component)}
+      `)
+    );
+    return node;
+  };
 
   return class extends Base {
     connectedCallback() {
-      if (super.connectedCallback) {
-        super.connectedCallback();
-      }
-      const name = this.localName;
+      super.connectedCallback();
       this.attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = `<${tag} class="${name}"><slot></slot></${tag}>`;
-      const css = `.${name} {${handler()}}`;
-      const style = document.createElement("style");
-      style.appendChild(document.createTextNode(css));
-      this.shadowRoot.appendChild(style);
+      this.shadowRoot.innerHTML = "<slot></slot>";
+      styleNode = createStyle(this);
+      this.shadowRoot.appendChild(styleNode);
+    }
+    update() {
+      if (styleNode && this.shadowRoot) {
+        const oldNode = styleNode;
+        styleNode = createStyle(this);
+        if (styleNode.isConnected) {
+          this.shadowRoot.replaceChild(styleNode, oldNode);
+        } else {
+          this.shadowRoot.appendChild(styleNode);
+        }
+      }
     }
   };
 };
@@ -47,9 +84,20 @@ export const withHandler = (name, handler) => Base =>
 export const withConnected = hanlder => Base =>
   class extends Base {
     connectedCallback() {
-      if (super.connectedCallback) {
-        super.connectedCallback();
-      }
+      super.connectedCallback();
       hanlder(this);
+    }
+  };
+
+export const withStore = handler => Base =>
+  class extends Base {
+    connectedCallback() {
+      super.connectedCallback();
+      const storeUpdateHandler = () => {
+        Object.assign(this, handler(store.getState(), this));
+        this.update();
+      };
+      store.subscribe(storeUpdateHandler);
+      storeUpdateHandler();
     }
   };
