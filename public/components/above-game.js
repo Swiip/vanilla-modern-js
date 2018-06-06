537be7d4d03fabe9be60b0becb376e5217002d63
Swiip <matthieu.lux@gmail.com>
2018-06-06 18:39:31 +0200

    refacto above game and grid

--- public/components/above-game.js
+++ public/components/above-game.js
@@ -1,42 +1,49 @@
-const template = `
-  <div class="above-game">
-    <p class="game-intro">Join the numbers and get to the <strong>2048 tile!</strong></p>
-    <a class="restart-button">New Game</a>
-  </div>
+import { component, withStyle, withMarkup } from "/framework/component.js";
 
-  <style>
-  .above-game {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
+component(
+  "swiip-above-game-container",
+  withStyle(
+    () => `
+      :host {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+      }
+    `
+  )
+);
 
-  .restart-button {
-    color: var(--light-text-white);
-    background-color: var(--heavy-bg-brown);
-    border-radius: 3px;
-    padding: 0 20px;
-    text-decoration: none;
-    color: #f9f6f2;
-    height: 40px;
-    cursor: pointer;
-    display: flex;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-  }
-  </style>
-`;
+component(
+  "swiip-restart-button",
+  withStyle(
+    () => `
+      :host {
+        color: var(--light-text-white);
+        background-color: var(--heavy-bg-brown);
+        border-radius: 3px;
+        padding: 0 20px;
+        text-decoration: none;
+        color: #f9f6f2;
+        height: 40px;
+        cursor: pointer;
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+      }
+    `
+  )
+);
 
-customElements.define(
+component(
   "swiip-above-game",
-  class AboveGame extends HTMLElement {
-    constructor() {
-      super();
-
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = template;
-    }
-  }
+  withMarkup(
+    () => `
+      <swiip-above-game-container>
+        <p>Join the numbers and get to the <strong>2048 tile!</strong></p>
+        <swiip-restart-button>New Game</swiip-restart-button>
+      </swiip-above-game-container>
+    `
+  )
 );
--- public/components/grid.js
+++ public/components/grid.js
@@ -1,71 +1,74 @@
 import { range } from "/utils/utils.js";
-
 import { render } from "/framework/render.js";
+import {
+  component,
+  withStyle,
+  withMarkup,
+  withProp
+} from "/framework/component.js";
 
 // im port { size } from "/game/conf.js";
 const size = 4;
 
-const template = `
-  <root></root>
+component(
+  "swiip-grid-container",
+  withStyle(
+    () => `
+      :host {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
 
-  <style>
-  .grid-container {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
+        display: grid;
+        grid-template-columns: repeat(4, 100px);
+        grid-template-rows: repeat(4, 100px);
+        grid-gap: 20px 20px;
+        justify-content: center;
+        align-content: center;
+      }
+    `
+  )
+);
 
-    display: grid;
-    grid-template-columns: repeat(4, 100px);
-    grid-template-rows: repeat(4, 100px);
-    grid-gap: 20px 20px;
-    justify-content: center;
-    align-content: center;
-  }
+component(
+  "swiip-grid-cell",
+  withProp("x"),
+  withProp("y"),
+  withStyle(
+    ({ x, y }) => `
+      :host {
+        position: absolute;
+        height: 100px;
+        width: 100px;
+        border-radius: 3px;
+        background-color: #cdc1b4;
+        grid-area: ${parseInt(x, 10) + 1} / ${parseInt(y, 10) + 1};
+      }
+    `
+  )
+);
 
-  .grid-cell {
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    border-radius: 3px;
-    background-color: #cdc1b4;
-  }
-  </style>
-`;
-
-customElements.define(
+component(
   "swiip-grid",
-  class Grid extends HTMLElement {
-    constructor() {
-      super();
-
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = template;
-
-      this.root = this.shadowRoot.querySelector("root");
-    }
-
-    connectedCallback() {
-      const newHtml = `
-        <div class="grid-container">
-          ${range(size)
-            .map(x =>
-              range(size)
-                .map(
-                  y =>
-                    `<div
-                      class="grid-cell"
-                      style="grid-area: ${x + 1} / ${y + 1};">
-                    </div>`
-                )
-                .join("")
-            )
-            .join("")}
-        </div>
-      `;
-
-      render(this.root, newHtml);
-    }
-  }
+  withMarkup(
+    () => `
+      <swiip-grid-container>
+        ${range(size)
+          .map(x =>
+            range(size)
+              .map(
+                y =>
+                  `
+                    <swiip-grid-cell x="${x}" y="${y}">
+                    </swiip-grid-cell>
+                  `
+              )
+              .join("")
+          )
+          .join("")}
+      </swiip-grid-container>
+    `
+  )
 );
