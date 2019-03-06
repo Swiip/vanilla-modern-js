18cf6b5f68cea07cd9cdc2e39d5a5b4da12b04a1
Swiip <matthieu.lux@gmail.com>
2019-03-06 10:46:42 +0100

    cleanup temporary code

--- public/wasm/index.js
+++ public/wasm/index.js
@@ -16,34 +16,3 @@ const init = async () => {
     throw e;
   }
 };
-
-const run = async () => {
-  try {
-    const { instance } = await WebAssembly.instantiateStreaming(
-      fetch("/rust/debug/2048.wasm"),
-      {}
-    );
-    console.log("WebAssembly instantiate success", instance, instance.exports);
-    const reducer = wrap(instance, "reducer");
-    const state = {
-      board: { current_id: 0, grid: [] },
-      changed: false,
-      won: false,
-      lost: false
-    };
-    const action = {
-      action_type: "Init",
-      direction: null,
-      random_value: null,
-      random_position: null
-    };
-    console.log("2048 state and action", state, action);
-    const newState = reducer({ state, action });
-    console.log("2048 state after Rust reduce", newState);
-    console.log("WebAssembly is awesome!");
-  } catch (error) {
-    console.log("WebAssembly instantiate failed", error);
-  }
-};
-
-// run();
